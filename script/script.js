15e8060846c50fe8f02e06bca348bdd2437dcd40
kckiran-git <kckiran0690@gmail.com>
2021-07-13 14:50:31 +0545

    body almost completed[C

---
+++ script/script.js
@@ -0,0 +1,10 @@
+function toggle() {
+	var sign = document.querySelector(".signup");
+
+	if( sign.style.display == "inline")
+	{
+		sign.style.display = "none"
+	}else{
+		sign.style.display ="inline"
+	}
+}
