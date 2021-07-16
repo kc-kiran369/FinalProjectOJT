1c639ef5b5948f89b32353a15af96c934315d88f
kckiran-git <kckiran0690@gmail.com>
2021-07-16 16:52:00 +0545

    backgroung toggle added

--- script/script.js
+++ script/script.js
@@ -8,3 +8,17 @@ function toggle() {
 		sign.style.display ="inline"
 	}
 }
+
+
+var toggles = true;
+function changeImg(){
+	var currentImage = document.getElementById('wallpaper').src;
+
+	if(toggles === true){
+		document.getElementById('wallpaper').src = "imgs/images/mobile1.png";
+	}
+	else{
+		document.getElementById('wallpaper').src = "imgs/images/laptop1.png";
+	}
+	toggles = !toggles;
+}
