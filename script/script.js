bf8c9432e12df41cbedb6d7ece8f22299331b18d
kckiran-git <kckiran0690@gmail.com>
2021-07-17 12:49:19 +0545

    updated

--- script/script.js
+++ script/script.js
@@ -1,21 +1,40 @@
+//function for toggling between signup and login section 
 function toggle() {
 	var sign = document.querySelector(".signup");
+	var signin = document.querySelector(".signin");
 
-	if( sign.style.display == "inline")
-	{
-		sign.style.display = "none"
-	}else{
-		sign.style.display ="inline"
+	if( signin.style.display != "inline" ){
+		if(sign.style.display == "inline")
+		{
+			sign.style.display = "none"
+		}else{
+			sign.style.display ="inline"
+		}
 	}
 }
 
 
+// function for hiding signup section 
+function hideSign(){
+	var sign = document.querySelector(".signup");
+	var signin = document.querySelector(".signin");
+	if( sign.style.display == "inline")
+	{
+		sign.style.display = "none"
+		signin.style.display ="inline"
+	}else{
+		sign.style.display = "inline"
+		signin.style.display ="none"
+	}	
+}
+
+
+
+// function for changing image of front page
 var toggles = true;
 function changeImg(){
-	var currentImage = document.getElementById('wallpaper').src;
-
 	if(toggles === true){
-		document.getElementById('wallpaper').src = "imgs/images/mobile1.png";
+		document.getElementById('wallpaper').src = "imgs/images/mobile2.png";
 	}
 	else{
 		document.getElementById('wallpaper').src = "imgs/images/laptop1.png";
