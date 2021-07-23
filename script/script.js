73001f068aa2e40a82a4237bdf52323593791100
kckiran-git <kckiran0690@gmail.com>
2021-07-23 15:48:58 +0545

    updated again

--- script/script.js
+++ script/script.js
@@ -1,12 +1,15 @@
 var name = "none";
 
+
 // function for opening signup menus
 function openmenu(){
 	name = localStorage.getItem("name");
 
+	//open signup section if name is none(default) or null
 	if(name=="none" || name==" "){
 			toggle();
 	}
+	//else enable user has signed in
 	else{
 		hasSignedin();
 	}
@@ -15,27 +18,25 @@ function openmenu(){
 
 //function for toggling between signup and login section 
 function toggle() {
-	var sign = document.querySelector(".signup");
-	var signin = document.querySelector(".signin");
 
-	if( signin.style.display != "inline"){
-		if(sign.style.display == "inline")
-		{
-			sign.style.display = "none"
-		}else{
-			sign.style.display ="inline"
-		}
-	}
-	else{
-		signin.style.display = "none"
+	//get signup section for entering info
+	var sign = document.querySelector(".signup");
+	
+	if(sign.style.display == "inline")
+	{
+		sign.style.display = "none"
+	}else{
+		sign.style.display ="inline"
 	}
 }
+
 
 //function for display <name> has signed in
 function hasSignedin(){
 	var hasSignVar = document.querySelector(".hasSigned");
 	var displayText = name +" has signed in";
 	document.querySelector(".hasSignText").innerHTML = displayText;
+
 	if(name!=" ")
 	{
 		if(hasSignVar.style.display == "inline")
@@ -52,12 +53,14 @@ function hasSignedin(){
 //actually changes value to none
 function deleteInfo() {
 
+	//set all info to none
 	localStorage.setItem("name" , "none");
 	localStorage.setItem("Fname" ,"none");
 	localStorage.setItem("Lname" ,"none");
 	localStorage.setItem("email" ,"none");
 	localStorage.setItem("password" ,"none");
 
+	//popup signup section and disable user has signed in window
 	document.querySelector(".hasSigned").style.display = "none"
 	document.querySelector(".signup").style.display = "inline"
 }
@@ -81,31 +84,33 @@ function signup(){
 		localStorage.setItem("Lname" , Lname);
 		localStorage.setItem("email" , email);
 		localStorage.setItem("password" , password);
+
 		document.querySelector(".signup").style.display = "none"
-		// document.querySelector(".hasSigned").style.display = "inline"
-		openmenu();
+		openmenu();	
 	}
 }
 
-// function for hiding signup section 
-function hideSign(){
-	var sign = document.querySelector(".signup");
-	var signin = document.querySelector(".signin");
-	if( sign.style.display == "inline")
-	{
-		sign.style.display = "none"
-		signin.style.display ="inline"
-	}else{
-		sign.style.display = "inline"
-		signin.style.display ="none"
-	}	
-}
 
+// <<<____unused code_____>>>
+// function for hiding signup section 
+// function hideSign(){
+// 	var sign = document.querySelector(".signup");
+// 	var signin = document.querySelector(".signin");
+// 	if( sign.style.display == "inline")
+// 	{
+// 		sign.style.display = "none"
+// 		signin.style.display ="inline"
+// 	}else{
+// 		sign.style.display = "inline"
+// 		signin.style.display ="none"
+// 	}	
+// }
 
 
 // function for changing image of front page
 var toggles = true;
 function changeImg(){
+
 	if(toggles === true){
 		document.getElementById('wallpaper').src = "imgs/images/mobile3.png";
 	}
@@ -116,11 +121,27 @@ function changeImg(){
 }
 
 
-
+function hide_cookie_section() {
+	document.querySelector(".cookieT").style.display = "none"
+}
 
 
 //for credit card information
-
 function view_purchase_section() {
-	alert("yup its working");
+	
+	var temp = document.querySelector(".purchase-section");
+	var cr = document.querySelector(".Cnum").value;
+
+	if(cr.length != 0)
+	{
+		if(temp.style.display == "inline")
+		{
+			temp.style.display = "none"
+			window.location.href = '../index.html';
+		}
+		else
+		{
+			temp.style.display = "inline"
+		}
+	}
 }
